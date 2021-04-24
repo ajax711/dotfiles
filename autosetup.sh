661055f4cce4d402d24a99e77581bb4d7c453e2b
Shobhit singh <singhshobhit2000@gmail.com>
2021-04-24 21:48:15 +0530

    just a little update

--- autosetup.sh
+++ autosetup.sh
@@ -1,32 +1,33 @@
-#!/bin/bash
 
 cd ~ 
 
 sudo apt-get update 
 
-wget http://0x0.st/-AGo.xml
-
-mv -AGo.xml xfce4-keyboard-shortcuts.xml 
+wget -c http://0x0.st/-AGo.xml -O xfce4-keyboard-shortcuts.xml
+ 
 mv xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 
 
-wget -o https://zoom.us/client/latest/zoom_amd64.deb
+wget https://zoom.us/client/latest/zoom_amd64.deb
 
-wget -o discord.deb “https://discord.com/api/download?platform=linux&format=deb”
+#wget -o discord.deb “https://discord.com/api/download?platform=linux&format=deb”
 
-wget -o 0x0.st/-A0J.deb //stremio
+wget  0x0.st/-A0J.deb //stremio -O stremio.deb
 
-sudo dpkg -i zoom_amd64.deb discord.deb -A0J.deb 
+sudo dpkg -i zoom_amd64.deb  stremio.deb
+
+sudo apt-get install -f 
+
+sudo dpkg -i zoom_amd64.deb  stremio.deb
 
 wget http://0x0.st/-A0v.xz //arduino 
 
 tar -xvf -A0v.xz
 
-touch /usr/bin/arduino
-file = /usr/bin/arduino
-printf "cd ~/arduino-nightly \n ./arduino" >> $file
+sudo touch /usr/bin/arduino
+sudo printf "cd ~/arduino-nightly \n ./arduino" >> /usr/bin/arduino
 
-chmod a+x /usr/bin/arduino 
+sudo chmod a+x /usr/bin/arduino 
 
 sudo apt-get install steam
 
