7e2aadd49f1df518b097c26c5c2b9a7c744a178d
ajax <singhshobhit2000@gmail.com>
2021-06-12 12:33:51 +0530

    bla bla

--- autosetup.sh
+++ autosetup.sh
@@ -10,19 +10,21 @@ mv xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4
 
 wget https://zoom.us/client/latest/zoom_amd64.deb
 
-#wget -o discord.deb “https://discord.com/api/download?platform=linux&format=deb”
+wget "https://discord.com/api/download?platform=linux&format=deb"
 
 wget  0x0.st/-A0J.deb //stremio -O stremio.deb
 
-sudo dpkg -i zoom_amd64.deb  stremio.deb
+mv download\?platform\=linux\&format\=deb discord.deb
+
+sudo dpkg -i zoom_amd64.deb  stremio.deb discord.deb 
 
 sudo apt-get install -f 
 
-sudo dpkg -i zoom_amd64.deb  stremio.deb
+sudo dpkg -i zoom_amd64.deb  stremio.deb discord.deb
 
 wget http://0x0.st/-A0v.xz //arduino 
 
-tar -xvf -A0v.xz
+tar -xvf -A0v.xz --directory ~/
 
 sudo touch /usr/bin/arduino
 sudo printf "cd ~/arduino-nightly \n ./arduino" >> /usr/bin/arduino
@@ -33,11 +35,22 @@ sudo apt-get install steam
 
 sudo apt-get install git 
 
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+
+~/.bash_it/install.sh
+
 git clone https://github.com/ajax711/dotfiles
+
+sudo apt-get install kitty 
+
+mkdir ~/.config/kitty
+cp ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf 
+
+git config --global user.email singhshobhit2000@gmail.com
+git config --global user.name bigass
 
 cp dotfiles/bash_aliases ~/
 mv bash_aliases .bash_aliases 
 source .bash_aliases
-
 
 
